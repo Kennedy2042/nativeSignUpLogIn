eaf3e47e3d576a88eb08ee4a4b56c0122c0737b1
Kennedy2042 <ogbonnakenedy@gmail.com>
2025-05-19 12:26:32 +0100

    tailwind instelled

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-xl font-bold">Edit app/index.tsx to edit this screen. yaga</Text>
+    </View>
+  );
+}
